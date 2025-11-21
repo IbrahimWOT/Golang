28705be09e8646125707c47fce1d657a2505629d
IbrahimWOT <ibrahimhridoy4848@gmail.com>
2025-11-22 02:13:28 +0600

    feat: Add file with varibale declare

---
+++ variable_dec.go
@@ -0,0 +1,11 @@
+package main
+import "fmt"
+
+func main(){
+	a:= 445 // it takes automatic datatype
+	var x float32 = 445.50
+	var y ="Hello"// it take automatics 
+	fmt.Println(a)
+	fmt.Println(x)
+	fmt.Println(y)
+}
